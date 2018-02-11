5e06bb06e0d9bf528bfe56faec03b93b3f7e675a
playbar <hgl868@126.com>
2018-02-11 10:32:41 +0800

    delete unused code

--- platform/darwin/src/MGLRasterSource_Private.h
+++
@@ -1,8 +0,0 @@
-#import "MGLRasterSource.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface MGLRasterSource (Private)
-@end
-
-NS_ASSUME_NONNULL_END
--- platform/node/test/render.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const suite = require('../../../mapbox-gl-js/test/integration').render;
-const suiteImplementation = require('./suite_implementation');
-const ignores = require('./ignores.json');
-
-suite.run('native', ignores, suiteImplementation);
