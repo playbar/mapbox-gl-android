66e95389f53e5b406889422fe80abdef21631f6f
playbar <hgl868@126.com>
2018-02-11 13:57:07 +0800

    change file position

---
+++ mason_packages/headers/jni.hpp/3.0.0/include/jni/field.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <jni/functions.hpp>
+#include <jni/class.hpp>
+#include <jni/object.hpp>
+#include <jni/type_signature.hpp>
+#include <jni/tagging.hpp>
+
+namespace jni
+   {
+    template < class TheTag, class T >
+    class Field
+       {
+        private:
+            jfieldID& field;
+
+        public:
+            using TagType = TheTag;
+
+            Field(JNIEnv& env, const Class<TagType>& clazz, const char* name)
+              : field(GetFieldID(env, clazz, name, TypeSignature<T>()()))
+               {}
+
+            operator jfieldID&() const { return field; }
+       };
+   }
